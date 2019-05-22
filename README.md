95a9bc3e1aa63108f1dda1b96c5b33e86651f553
LuckyP86H <luckypaulxu@gmail.com>
2019-05-21 20:05:51 -0400

    travis ci

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # sample-travis-heroku-02
-lnknjhb
--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 
 const app = express();
 
-// travis use 3000 as default 
+// travis use 3000 as default
 app.get('/', (req, res) => {
-    // eslint-disable-next-line no con
-    console.log('req = \n', req);
-    res.send('Test CI with Travis');
+  // eslint-disable-next-line no con
+  console.log('req = \n', req);
+  res.send('Test CI with Travis Hi');
 });
 
 const server = app.listen(3001, () => {
-    // eslint-disable-next-line no-console
-    console.log('app running on port 3001');
+  // eslint-disable-next-line no-console
+  console.log('app running on port 3001');
 });
 
 // eslint-disable-next-line no-console
